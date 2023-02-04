942f7d70f22a32ca55ee5f784dbc2b8d51f13473
DanielBrisch <124108211+DanielBrisch@users.noreply.github.com>
2023-02-04 18:10:45 -0300

    commit-1

--- Main.dart
+++ Main.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('Hello world!');
+  int idade = 19;
+  String Nome = 'Daniel';
+  double altura = 1.86;
+  bool Boolean = true;
+  bool igual = (altura != idade);
+  String resp;
+
+  if (Boolean == true) {
+    resp = 'sim';
+  } else {
+    resp = 'não';
+  }
+
+  print('Meu nome é $Nome e eu tenho $idade anos, eu estou começando com Dart? $resp!!! e eu tenho $altura de altura');
 }
