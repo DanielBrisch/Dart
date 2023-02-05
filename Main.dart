bfc5747921ff81cb55f1537f156bc4723db26177
DanielBrisch <124108211+DanielBrisch@users.noreply.github.com>
2023-02-05 16:26:47 -0300

    commit-3

--- Main.dart
+++ Main.dart
@@ -6,6 +6,7 @@ void main() {
   bool igual = (altura != idade);
   String resp;
   final String apelido;
+  int energia = 100;
 
   apelido = 'DBC';
 
@@ -15,12 +16,28 @@ void main() {
     resp = 'não';
   }
 
+  print('Meu nome é $Nome e eu tenho $idade anos, eu estou começando com Dart? $resp!!! e eu tenho $altura de altura');
   List<dynamic> Info = [idade,altura,resp,Nome,apelido];
 
   print('Meu nome é ${Info[3]} \n'
       'e eu tenho ${Info[0]} anos, \n'
       'eu estou começando com Dart? $resp!!! \n'
       'e eu tenho ${Info[1]} de altura');
+
+
+  for (int i =1; i<=5; i++) {
+    print('concluir $i Voltas');
+  }
+
+  while(energia > 0) {
+    print('mais uma repetição, $energia');
+    energia -= 10;
+  }
+  do  {
+    print('mais uma repetição');
+    energia = -10;
+  }while (energia>0);
+
 
 
 }
